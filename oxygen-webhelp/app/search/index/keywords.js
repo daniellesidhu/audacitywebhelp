a870c58d6f2a90c98b59905e3c41f89e11037d99
daniellesidhu <92005327+daniellesidhu@users.noreply.github.com>
2022-04-11 01:47:28 -0500

    Add files via upload

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,18 @@
+define(function() {var keywords=[{w:"Analyzers",p:["p0"]},{w:"Effects",p:["p1"]},{w:"Exporting",p:["p2","p5"]},{w:"Audio",p:["p2","p5","p7"]},{w:"Features",p:["p3"]},{w:"Getting",p:["p4"]},{w:"Started",p:["p4"]},{w:"Importing",p:["p5","p7"]},{w:"and",p:["p5"]},{w:"Tools",p:["p6"]},{w:"Audacity",p:["p8"]},{w:"Introduction",p:["p8"]},{w:"Generators",p:["p9"]},{w:"What",p:["p10"]},{w:"is",p:["p10"]},{w:"Audacity?",p:["p10"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2, 3];
+ph["p3"]=[4];
+ph["p4"]=[5, 6];
+ph["p5"]=[7, 8, 2, 3];
+ph["p6"]=[9];
+ph["p7"]=[7, 3];
+ph["p8"]=[10, 11];
+ph["p9"]=[12];
+ph["p10"]=[13, 14, 15];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {2:5,9:3,6:3,0:3,8:-1,3:10,7:5,10:8,5:4,1:3,4:8};
+});
